c48a6bf37169a5ace1a809d0776ee5912e956d43
afeiship <1290657123@qq.com>
2017-07-26 11:07:07 +0800

    issue: update dist scripts

--- build/clean.js
+++ build/clean.js
@@ -3,14 +3,13 @@
   'use strict';
 
   var gulp = require('gulp');
-  var fs = require('fs');
   var $ = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del']
   });
 
-  //import
-  fs.readdirSync('./build').map(function(file) {
-    require('./build/' + file);
+  //clean
+  gulp.task('clean',function() {
+    return $.del('dist');
   });
 
 }());
--- build/scripts.js
+++ build/scripts.js
@@ -3,14 +3,15 @@
   'use strict';
 
   var gulp = require('gulp');
-  var fs = require('fs');
   var $ = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del']
   });
 
-  //import
-  fs.readdirSync('./build').map(function(file) {
-    require('./build/' + file);
+
+  //scripts
+  gulp.task('scripts', function() {
+    return gulp.src('src/components/*.*')
+      .pipe(gulp.dest('dist'));
   });
 
 }());
